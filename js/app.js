175bdde2dd68e8db2871020db4edf37cd9700170
mahfuz091 <engmahfuz14@gmail.com>
2023-03-03 02:55:54 +0600

    added load & show modal script

--- js/app.js
+++ js/app.js
@@ -64,6 +64,72 @@ const showAllDataTogether = () => {
       });
   };
 
+  const loadSingleData =(id)=>{
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>showSingleData(data.data))
+
+}
+const showSingleData=(data)=>{
+    console.log(data)
+    const {description, pricing, features }=data
+    const modalBody=document.getElementById('modal-body')
+    modalBody.innerHTML="";
+    modalBody.innerHTML += `
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+    <div class="col ">
+      <div class="card modal-feature">
+        <h5 class="title">${description}</h5>
+        <div class="card-body">
+          <div class="d-flex gap-2">
+            <div class="modal-card">${pricing? pricing[0].price : 'Free Of Cost'}<br>
+                ${pricing?pricing[0].plan : "Basic"}</div>
+            <div class="modal-card">${pricing?pricing[1].price : 'Free Of Cost'}<br>
+            ${pricing?pricing[1].plan : "Pro"}</div>
+            <div class="modal-card">${pricing?pricing[2].price: "Free of Cost/"}<br>
+            ${pricing?pricing[2].plan:"Enterprise"}</div>
+
+          </div>
+          <div class="d-flex mt-3">
+            <div class="features">
+                <h5>Features</h5>
+                <ul>
+                    <li>${features[1].feature_name}</li>
+                    <li>${features[2].feature_name}</li>
+                    <li>${features[3].feature_name}</li>
+
+                </ul>
+            </div>
+            <div class="integrations">
+                <h5>Integrations</h5>
+                <ul>
+                    <li>Customizable responses</li>
+                    <li>Multilingual support</li>
+                    <li>Seamless integration</li>
+
+                </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="images/card2.png" class="card-img-top" alt="...">
+        <p class="accuracy">94% accuracy</p>
+        <div class="card-body">
+          <h5 class="card-title text-center">Hi, how are you doing today?</h5>
+          <p class="card-text text-center">I'm doing well, thank you for asking. How can I assist you today?</p>
+        </div>
+      </div>
+    </div>
+    
+    
+</div>
+    `
+}
+
 
 
 
