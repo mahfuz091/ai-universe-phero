7b5f6c6387a09065b255f0c0660a3ec930cc26c2
mahfuz091 <engmahfuz14@gmail.com>
2023-03-03 02:41:29 +0600

    added show all data button & javascript

--- js/app.js
+++ js/app.js
@@ -53,7 +53,17 @@ const showAllData =(data)=>{
     });
     
 
-}
+};
+const showAllDataTogether = () => {
+    const url = "https://openapi.programming-hero.com/api/ai/tools"
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        //   console.log(data);
+        showAllData(data.data.tools);
+      });
+  };
+
 
 
 
